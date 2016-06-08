1110e7ab2bfda16c0a2be043651c7000059ad1f9
daniel-apolinario <drfapolinario@gmail.com>
2016-06-08 15:39:51 -0300

    Update server.R

--- server.R
+++ server.R
@@ -28,7 +28,7 @@ server <- function(input,output) {
       
       # Write to a file specified by the 'file' argument
       registerIndex = which(datasource$V2 == input$coordenadaID)
-      writeLines(c(paste0(datasource[registerIndex, 1], datasource[registerIndex, 2], datasource[registerIndex, 3]), paste(datasource[registerIndex, 4]), paste(datasource[registerIndex, 5]), paste(datasource[registerIndex, 6]), paste(datasource[registerIndex, 7]), paste(datasource[registerIndex, 8]), paste(datasource[registerIndex, 9]), paste(datasource[registerIndex, 10]), paste(datasource[registerIndex, 11]), paste(datasource[registerIndex, 12]), paste(datasource[registerIndex, 13]), paste(datasource[registerIndex, 14])), file, sep = "\n")
+      writeLines(c(paste0(datasource[registerIndex, 1], datasource[registerIndex, 2], datasource[registerIndex, 3]), paste(datasource[registerIndex, 4]), paste(datasource[registerIndex, 5]), paste(datasource[registerIndex, 6]), paste(datasource[registerIndex, 7]), paste(datasource[registerIndex, 8]), paste(datasource[registerIndex, 9]), paste(datasource[registerIndex, 10]), paste(datasource[registerIndex, 11]), paste(datasource[registerIndex, 12]), paste(datasource[registerIndex, 13]), paste(datasource[registerIndex, 14])), file, sep = "\r\n")
     }
   )
 }
