fb08c485bb6ec7f711fc6eebb89fd45ebb7cbb5a
Flavio Barros <flaviomargarito@gmail.com>
2016-06-24 15:04:55 -0300

    Comentários gerais para facilitar o entendimento do texto.

--- ui.R
+++ ui.R
@@ -1,13 +1,46 @@
 ui <- fluidPage (
   
-    titlePanel('Escolha uma coordenada geografica'),
+  ## Título da página  
+  titlePanel('Escolha uma coordenada geografica'),
+    
+    ## Definição do layout com barra lateral
     sidebarLayout(
+      
+      ## Definições da ui para a barra lateral
       sidebarPanel(
+        
+        ## Input para arquivo
+        fileInput('file1', 'Escolha um arquivo CSV',
+                  accept=c('text/csv', 
+                           'text/comma-separated-values,text/plain', 
+                           '.csv')),
+        tags$hr(),
+        
+        ## Checkbox para o arquivo com cabeçalho ou não
+        checkboxInput('header', 'Cabeçalho', TRUE),
+        
+        ## Indicação para separador
+        radioButtons('sep', 'Separador',
+                     c(Comma=',',
+                       Semicolon=';',
+                       Tab='\t'),
+                     ','),
+        
+        ## Indicação de quota
+        radioButtons('quote', 'Quote',
+                     c(None='',
+                       'Double Quote'='"',
+                       'Single Quote'="'"),
+                     '"'),
+        
+        
         numericInput("latitude", label = "Latitude", value=0),
         numericInput("longitude", label = "Longitude", value=0),
         textInput("coordenadaID", label = "ID da Coordenada"),
         downloadButton('downloadData', 'Download')
       ),
+      
+      ## Painel principal
       mainPanel(
        textOutput("latitudeSelecionada"),
        textOutput("longitudeSelecionada")
